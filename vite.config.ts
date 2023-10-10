407885f3d99ad132ae00e1c86667bceee2596051
Xifo <xifo.wu@gmail.com>
2023-10-10 13:49:49 +0800

    chore: 调整 config，产品环境时使用 cdn 加速，减少打包体积

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,49 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { cdn } from 'vite-plugin-cdn2';
 import swc from 'rollup-plugin-swc3';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    swc({
-      include: /\.[mc]?[jt]sx?$/,
-      exclude: /node_modules/,
-      tsconfig: 'tsconfig.json',
-      jsc: {},
-    }),
-    cdn({ modules: ['react'] }),
-  ],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  if (mode === 'development') {
+    return {
+      server: {
+        host: '0.0.0.0',
+        proxy: {
+          '/api': {
+            target: env.VITE_API_BASE_URL,
+            changeOrigin: true,
+          }
+        },
+      },
+      plugins: [
+        swc({
+          include: /\.[mc]?[jt]sx?$/,
+          exclude: /node_modules/,
+          tsconfig: 'tsconfig.json',
+          jsc: {},
+        }),
+      ],
+    };
+  }
+
+  return {
+    plugins: [
+      swc({
+        include: /\.[mc]?[jt]sx?$/,
+        exclude: /node_modules/,
+        tsconfig: 'tsconfig.json',
+        jsc: {},
+      }),
+      cdn({
+        url: 'https://cdn.bootcdn.net/ajax/libs/',
+        modules: [{ name: 'react', relativeModule: 'umd/react.production.min.js' }],
+        apply: command,
+        resolve(baseURL, { name, version, relativeModule }) {
+          return new URL(`${name}/${version}/${relativeModule}`, baseURL).href;
+        },
+      }),
+    ],
+  };
 });
